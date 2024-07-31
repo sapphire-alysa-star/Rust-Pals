3cae6fed28c4189ec35e4647413bbf92f787effb
Sapphire Star <sapphire.alysa.star@gmail.com>
2024-07-31 14:55:22 -0700

    add explicit slice logic

--- src/bin/vector.rs
+++ src/bin/vector.rs
@@ -1,9 +1,12 @@
+// Important! Other tutorials and explanations will talk about 'borrowing' and the 'borrow checkers'. As will errors.
+// The borrow checker is the thing that prevents memory errors from occuring at runtime. It checks at compile time.
+
 // Lets learn about vectors and do some problems!
 
 // If I comment right after or next to a print I am posted the output!
 
 fn main() {
-    let one_two_three = vec![1, 2, 3, 4];
+    let one_two_three: Vec<i32> = vec![1, 2, 3, 4];
     println!("{:?}", one_two_three); // [1, 2, 3, 4]
 
     let third: &i32 = &one_two_three[2];
@@ -49,10 +52,25 @@ fn main() {
     x[4] = 5; // set value directly
 
     let mut sum = 0;
-    for i in x {
-        sum += i;
+    for i in &x {
+        sum += *i;
     }
     println!("The sum of elements is: {}", sum); // The sum of elements is: 15
 
+    // lets do this with a function
+    println!("The sum of elements is still: {}", vector_sum(&x));
 
 }
+
+fn vector_sum(v: &Vec<i32>) -> i32 {
+    let mut sum = 0;
+    for i in v {
+        sum += *i;
+    }
+
+    sum
+}
+
+// Problem Set: Assume all vectors
+
+// 
