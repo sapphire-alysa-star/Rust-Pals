175d87c411a61d9074318a1eeaa6fbde79119a5e
Sapphire Star <sapphire.alysa.star@gmail.com>
2024-08-02 23:49:36 -0700

    get and set value for Celene

--- src/bin/12-impl.rs
+++ src/bin/12-impl.rs
@@ -1,30 +1,34 @@
-struct Val {
+struct Pair<'a> {
+    name: &'a str,
     value: f64,
 }
+
 // impl of Val
-impl Val {
+impl<'a> Pair<'a> {
     fn get_value(&self) -> &f64 {
         &self.value
     }
-}
 
-// T is the parameter for a generic type. 
-struct GenVal<T> {
-    value: T,
-}
+    // fn set_value(&self) -> Pair {
+    //     &self.value = value
+    // }
 
-// impl of GenVal for a generic type `T`
-impl<T> GenVal<T> {
-    fn get_value(&self) -> &T {
-        &self.value
+    fn get_name(&self) -> &str {
+        &self.name
     }
+
+    // fn set_name(&self, new_name: &str) -> Pair {
+    //     let copied_name = String::from(new_name);
+    //     &self.name = &copied_name[0..];
+    // }
 }
 
 fn main() {
-    let x = Val { value: 3.0 };
-    
-    let three: i32 = 3;
-    let y = GenVal { value: three };
 
-    println!("{}, {}", x.get_value(), y.get_value());
+    let mut p1 = Pair {
+        name: "Celene",
+        value: -100.0
+    };
+
+    println!("Name: {}. Value: {}.", p1.get_name(), p1.get_value());
 }
