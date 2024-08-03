55c6d4441e72b31d0c484ba4395c9809365ec070
Sapphire Star <sapphire.alysa.star@gmail.com>
2024-08-02 23:19:07 -0700

    pretty good my option

--- src/bin/12-impl.rs
+++ src/bin/12-impl.rs
@@ -1,30 +1,30 @@
 struct Val {
-    val: f64,
+    value: f64,
 }
 // impl of Val
 impl Val {
-    fn value(&self) -> &f64 {
-        &self.val
+    fn get_value(&self) -> &f64 {
+        &self.value
     }
 }
 
 // T is the parameter for a generic type. 
 struct GenVal<T> {
-    gen_val: T,
+    value: T,
 }
 
 // impl of GenVal for a generic type `T`
 impl<T> GenVal<T> {
-    fn value(&self) -> &T {
-        &self.gen_val
+    fn get_value(&self) -> &T {
+        &self.value
     }
 }
 
 fn main() {
-    let x = Val { val: 3.0 };
+    let x = Val { value: 3.0 };
     
     let three: i32 = 3;
-    let y = GenVal { gen_val: three };
+    let y = GenVal { value: three };
 
-    println!("{}, {}", x.value(), y.value());
+    println!("{}, {}", x.get_value(), y.get_value());
 }
---
+++ src/bin/13-my-option.rs
@@ -0,0 +1,76 @@
+// Self is the type of the current object. 
+// It may appear either in a trait or an impl, 
+// but appears most often in trait where it is a stand-in for whatever type will end up implementing the trait (which is unknown when defining the trait):
+
+// impl Clone for MyType {
+//     // I can use either the concrete type (known here)
+//     fn clone(&self) -> MyType;
+
+//     // Or I can use Self again, it's shorter after all!
+//     fn clone(&self) -> Self;
+// }
+
+// if using self, the function introduced is a method
+// if using any other name, the function introduced is an associated function
+
+enum MyOption<T> {
+    MySome {value: T},
+    MyNone
+}
+
+impl<T> MyOption<T> {
+    // method since first argument is &self
+    fn unwrap(&self) -> &T {
+        match &self {
+            MyOption::MySome {value} => value,
+            MyOption::MyNone => panic!("Attempted to unwrap MyNone!")
+        }
+    }
+
+    fn is_some(&self) -> bool {
+        match &self {
+            MyOption::MySome {value: _v} => true,
+            MyOption::MyNone => false
+        }
+    }
+
+    // association function
+    pub fn some(value: T) -> MyOption<T> {
+        MyOption::MySome {
+            value: value
+        }
+    }
+
+    pub fn none() -> MyOption<T> {
+        MyOption::MyNone
+    }
+}
+
+// #[repr(C)]
+// struct MyNone<T> {
+//     _d: PhantomData<T>
+// }
+
+// impl<T> MyNone<T> {
+//     fn unwrap() -> std::option::Option<T> {
+//         None
+//     }
+// }
+
+
+fn main () {
+    let x1 = "Hello";
+    let x2 = "Celene";
+
+    let my_some_1 = MyOption::some(x1);
+    let my_some_2 = MyOption::some(x2);
+
+    for i in [&my_some_1, &my_some_2] {
+        if i.is_some() {
+            println!("{}", i.unwrap())
+        }
+    }
+
+}
+
+
