9547e041f6ba535f1d7d77ff97bcaddc2ce0b01c
sapph <sapphire.alysa.star@gmail.com>
2024-08-23 14:13:30 -0700

    refactor my string

---
+++ src/cpp/04-my-string.cpp
@@ -0,0 +1,46 @@
+// spec1_destructors.cpp
+#include <cstring> // strlen()
+#include <iostream>
+using namespace std;
+
+class String
+{
+    public:
+        String(const char* ch)
+        {
+            size_t sizeOfText = strlen(ch) + 1; // +1 to account for trailing NULL
+
+            // Dynamically allocate the correct amount of memory.
+            _text = new char[sizeOfText];
+
+            // If the allocation succeeds, copy the initialization string.
+            if (_text)
+            {
+                strcpy(_text, ch);
+            }
+        }
+
+        ~String()
+        {
+            // Deallocate the memory that was previously reserved for the string.
+            delete[] _text;
+        }
+
+        void print() {
+            // so we can check things actually work
+            cout << this->_text << endl;
+        }
+
+    private:
+        char* _text {nullptr};
+};
+
+int main()
+{
+    String str("We love C++");
+    str.print();
+}
+
+// This is RAII
+// You use new and delete exclusively in the constructor and destrucotr of classes.
+// This means new and delete are used correctly when stuff is on/off the stack. 
