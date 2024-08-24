1df391ebbd1ada342f927fd3d38c5abddaa3802d
sapph <sapphire.alysa.star@gmail.com>
2024-08-23 22:01:25 -0700

    refactor to still use loop on speed test

--- src/bin/27-speedy-heap.rs
+++ src/bin/27-speedy-heap.rs
@@ -14,8 +14,11 @@ fn big_allocation(val: u32, length: usize) -> Box<[u32]> {
         let p: *mut u8 = alloc(layout);
         let p32 = p as *mut u32;
 
+        for i in 0..length {
+            *p32.wrapping_add(i) = val;
+        }
         let slice = from_raw_parts_mut(p32, length);
-        slice.fill(val); // no loop sorry.
+        // slice.fill(val); // no loop sorry.
 
         Box::<[u32]>::from_raw(slice as *mut [u32])
 
@@ -26,23 +29,26 @@ fn big_allocation(val: u32, length: usize) -> Box<[u32]> {
 }
 
 fn main() {
-    let t1 = SystemTime::now();
-    let boxed_slice_1 = big_box(5, LENGTH);
-    let duration_1 = t1.elapsed();
+    let t_alloc = SystemTime::now();
+    let boxed_slice_alloc = big_allocation(7, LENGTH);
+    let duration_alloc = t_alloc.elapsed();
 
-    println!("Boxxed Slice One has first element {} and length {}", boxed_slice_1[0], boxed_slice_1.len());
-    println!("Time elapsed to run vec! version: {:?}", duration_1);
+    println!("Boxxed_Slice_Alloc has 99th element {} and length {}", boxed_slice_alloc[99], boxed_slice_alloc.len());
+    println!("Time elapsed to run alloc version: {:?}", duration_alloc);
 
-    let t2 = SystemTime::now();
-    let boxed_slice_2 = big_allocation(7, LENGTH + 920);
-    let duration_2 = t2.elapsed();
+    let t_vec = SystemTime::now();
+    let boxed_slice_vec = big_box(5, LENGTH);
+    let duration_vec = t_vec.elapsed();
 
-    println!("Boxxed Slice Two has 99th element {} and length {}", boxed_slice_2[99], boxed_slice_2.len());
-    println!("Time elapsed to run alloc version: {:?}", duration_2);
+    println!("Boxxed_Slice_Vec has first element {} and length {}", boxed_slice_vec[0], boxed_slice_vec.len());
+    println!("Time elapsed to run vec! version: {:?}", duration_vec);
 
-    // for u32 they run in the same time! Though variance. sometimes either wins the race
-    // Time elapsed to run vec! version: Ok(423.783211ms)
-    // Time elapsed to run alloc version: Ok(435.575445ms)
+    // Either they have similar speed. Or whichever runs second sometimes hits a speed bump.
+
+    // Boxxed_Slice_Alloc has 99th element 7 and length 83886080
+    // Time elapsed to run alloc version: Ok(651.759144ms)
+    // Boxxed_Slice_Vec has first element 5 and length 83886080
+    // Time elapsed to run vec! version: Ok(498.07131ms)
 }
 
 fn big_box(val: u32, length: usize) -> Box<[u32]> {
