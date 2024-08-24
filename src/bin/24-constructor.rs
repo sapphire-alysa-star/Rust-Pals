4ce321406f04a0806cb1554d061dd626b98493bb
sapph <sapphire.alysa.star@gmail.com>
2024-08-23 22:55:25 -0700

    Constructor example and defaults

--- src/bin/24-constructor.rs
+++ src/bin/24-constructor.rs
@@ -3,28 +3,68 @@
 // The Rust Destructor equivalent is the Drop trait you can implement
 
 #![allow(unused_variables)]
+#![allow(dead_code)]
 
-struct Droppable {
+pub struct Clock {
     name: &'static str,
+    seconds: u8,
+    hours: u8,
+    minutes: u8,
+}
+
+impl Clock {
+    fn new(name: &'static str, seconds: u8, hours: u8, minutes: u8) -> Self {
+        Self {
+            name,
+            seconds,
+            hours,
+            minutes
+        }
+    }
 }
 
 // This trivial implementation of `drop` adds a print to console.
-impl Drop for Droppable {
+impl Drop for Clock {
     fn drop(&mut self) {
-        println!("Dropping {}", self.name);
+        println!("Dropping da clock: {}", self.name);
+    }
+}
+
+impl Default for Clock {
+    fn default() -> Self {
+        Self { 
+            name: "",
+            seconds: 0,
+            hours: 0,
+            minutes: 0,
+        }
     }
 }
 
 fn stack_frame() {
-    let x = Droppable {
-        name: "Baller Man"
+    let x = Clock {
+        name: "Baller Man",
+        ..Default::default()
     };
+}
+
+fn another_frame() {
+    let x = Clock::new("Tik Tok", 0, 0, 0);
 }
 
 fn main() {
 
     stack_frame();
+    another_frame();
 
-    stack_frame();
+    // Dropping da clock: 
+    // Dropping da clock: Baller Man
+    // Dropping da clock: Tik Tok
+
+    // Note: Default::default() actually creates and then discards a new instance.
+
+    // To my knowlege there is no way to FORCE users to create a struct through new.
+    // So gotta be tricky with pointer defaults if doing low level. 
+    // However you do have a destructor hook to use to free memory
 
 }
